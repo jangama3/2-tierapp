68734aca3f99bcae0b601f7712ee3bb015f3132b
gabousmen <gabousmen@hotmail.com>
2024-04-09 18:31:02 -0400

    fgdfgdgd

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,10 +1,10 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  instance_tenancy = "default"
-  enable_dns_hostnames = true
-  enable_dns_support =  true
+#resource "aws_vpc" "main" {
+ # cidr_block = var.vpc_cidr
+  #instance_tenancy = "default"
+  #enable_dns_hostnames = true
+  #enable_dns_support =  true
 
  # tags = {
  #   Name = "${var.project_name}-vpc"
 #  }
-}
+#}
